8aa405c83d0d34abd34ba80e3bccdb87af0d464e
Abraham Broca Jiménez <brocakun@NOVO-MOBILE.(none)>
2011-09-14 15:56:02 -0500

    adding js for map

---
+++ sites/all/themes/onix/js/front.js
@@ -0,0 +1,29 @@
+(function ($) {
+
+  Drupal.behaviors.googlemap  = {
+    attach: function(context) {
+     var latlng = new google.maps.LatLng(25.6736,-100.337727);
+
+      var myOptions = {
+        zoom: 15,
+        center: latlng,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+      };
+      var map = new google.maps.Map(document.getElementById("mapa_onix"), myOptions);
+      var contentString = '<div class="infowindow"><b>OnixMedia</b> <br/>Matamoros 1528 Ote. Col. Obispado, Monterrey Nuevo Leon <br/>+(52) 8180135723 </div>';
+      var infowindow = new google.maps.InfoWindow({
+          content: contentString
+      });
+      var marker = new google.maps.Marker({
+          position: latlng,
+          map: map,
+          title:"OnixMedia (Monterrey)"
+      });
+      google.maps.event.addListener(marker, 'click', function() {
+        infowindow.open(map,marker);
+      });       
+      $("#mapa_onix").attr("position", "absolute");
+    }
+  };
+
+}(jQuery));
