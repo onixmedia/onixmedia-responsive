2288e5b82e4da81a7a0474b43e926cb413c31664
Abraham Broca Jiménez <brocakun@NOVO-MOBILE.(none)>
2011-09-14 09:49:05 -0500

    adding module and page contition

--- sites/all/themes/onix/templates/node--slideshow.tpl.php
+++ sites/all/themes/onix/templates/node--slideshow.tpl.php
@@ -82,14 +82,57 @@
  */
 ?>
 <div id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
+  <?php
+  //Looking wheter is a page or a teaser
+  if($page){
+  ?>
+  
+  <?php print $user_picture; ?>
 
-<div>
-<?
-print render($content['field_slideshow_image']);  
-//print render($node->field_slideshow_image);
-//print_r($node);
-?>
+  <?php print render($title_prefix); ?>
+  <?php if (!$page && $title): ?>
+    <h2<?php print $title_attributes; ?>><a href="<?php print $node_url; ?>"><?php print $title; ?></a></h2>
+  <?php endif; ?>
+  <?php print render($title_suffix); ?>
 
-</div>
+  <?php if ($unpublished): ?>
+    <div class="unpublished"><?php print t('Unpublished'); ?></div>
+  <?php endif; ?>
 
+  <?php if ($display_submitted): ?>
+    <div class="submitted">
+      <?php print $submitted; ?>
+    </div>
+  <?php endif; ?>
+
+  <div class="content"<?php print $content_attributes; ?>>
+    <?php
+      // We hide the comments and links now so that we can render them later.
+      hide($content['comments']);
+      hide($content['links']);
+      print render($content);
+    ?>
+  </div>
+
+  <?php print render($content['links']); ?>
+
+  <?php print render($content['comments']); ?>
+
+  <?php
+  } //end if($page)
+
+  //Looking wheter is a page or a teaser
+  if($teaser){
+  ?>
+  <div class="node-slideshow-front">
+  <?
+  print render($content['field_slideshow_image']);  
+  //print render($node->field_slideshow_image);
+  //print_r($node);
+  ?>
+  </div>
+
+  <?php
+  } //end if($teaser)
+  ?>
 </div><!-- /.node -->
